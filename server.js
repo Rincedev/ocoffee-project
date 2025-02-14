d6ddde9da3f0be4654a07eec002470f16dfc520c
Rincedev <nicolas.egrot@gmail.com>
2025-02-14 09:18:58 +0100

    feat: reCaptcha on contact form

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import session from 'express-session';
 import router from './app/router.js';
-import { exec } from "child_process";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
