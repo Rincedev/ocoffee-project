6347ad22ef7e19c3a973a2cdc8520aeab94ca62f
Rincedev <nicolas.egrot@gmail.com>
2025-02-14 11:47:02 +0100

    ef

--- server.js
+++ server.js
@@ -59,7 +59,11 @@ async function startServer() {
       cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 },
     });
   }
-  console.log("Session ID:", req.sessionID);
+  app.use((req, res, next) => {
+    console.log("Session ID:", req.sessionID);  // Vérifier si un ID de session est généré
+    next();
+  });
+  
   app.use(sessionMiddleware);
 
   // Configurer le moteur de rendu (EJS)
