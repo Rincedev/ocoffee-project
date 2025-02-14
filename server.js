f97c63a9c998e30af8fc07434b95f40cc93714d9
Rincedev <nicolas.egrot@gmail.com>
2025-02-14 09:36:38 +0100

    fix: session to redis on prod with new ENV

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import session from 'express-session';
 import router from './app/router.js';
+import { RedisStore } from "connect-redis";
+import { createClient } from "redis";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,22 +15,67 @@ export default __dirname;
 
 const app = express();
 
-app.use(session({
-  secret: process.env.SECRET_SESSION,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}));
+async function startServer() {
+  let sessionMiddleware;
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+  if (process.env.NODE_ENV === "production") {
+    // Création du client Redis en production
+    const redisClient = createClient({ url: process.env.REDIS_URL });
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+    redisClient.on("error", (err) => console.error("Redis Error:", err));
 
-app.use(localsMiddleware.listAllCategories);
+    await redisClient.connect();
 
-app.use(router);
+    const redisStore = new RedisStore({
+      client: redisClient,
+      disableTouch: true,
+    });
+
+    sessionMiddleware = session({
+      store: redisStore,
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: true, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 },
+    });
+  } else {
+    // Utilisation de la session Express classique en développement
+    sessionMiddleware = session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 * 24 },
+    });
+  }
+
+  app.use(sessionMiddleware);
+
+  // Configurer le moteur de rendu (EJS)
+  app.set("view engine", "ejs");
+  app.set("views", "views");
+
+  // Configurer un dossier d'assets statiques
+  app.use(express.static("public"));
+
+  // Ajout d'un body parser
+  app.use(express.urlencoded({ extended: true }));
+  app.use(localsMiddleware.listAllCategories);
+  // Brancher le routeur
+  app.use(router);
+
+  // Lancer un serveur
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(Server started at http://localhost:${port});
+  });
+}
+
+
+
+
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
