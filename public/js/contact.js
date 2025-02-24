917843fdec31f6fdb4ef1d08851fc1ebe314dcf6
Rincedev <nicolas.egrot@gmail.com>
2025-02-24 13:05:20 +0100

    fix: again some fixes in contact.js

--- public/js/contact.js
+++ public/js/contact.js
@@ -33,14 +33,16 @@
 
    // Envoi de l'email avec EmailJS
    emailjs.sendForm('contact_service', 'contact_form', this)
-     .then(function(response) {
-       console.log('SUCCESS!', response);
-       alert('Votre message a été envoyé avec succès !');
-
-       // Réinitialiser le formulaire après l'envoi réussi
-       document.getElementById("contact-form").reset();
-     }, function(error) {
-       console.log('FAILED...', error);
-       alert("Erreur lors de l'envoi du message. Veuillez réessayer.");
-     });
+        .then(function(response) {
+          console.log('SUCCESS!', response);
+          alert('Votre message a été envoyé avec succès !');
+          
+          // Réinitialiser les champs du formulaire après l'envoi réussi
+          document.querySelector('input[name="user_name"]').value = '';
+          document.querySelector('input[name="user_email"]').value = '';
+          document.querySelector('textarea[name="message"]').value = '';
+        }, function(error) {
+          console.log('FAILED...', error);
+          alert('Erreur lors de l\'envoi du message. Veuillez réessayer.');
+        });
  });
